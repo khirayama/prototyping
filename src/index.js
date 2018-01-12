84b1d90067492e20942704171cf6489a6d9b0463
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-12 23:08:35 +0900

    Add snapshot feature

--- src/index.js
+++ src/index.js
@@ -83,6 +83,43 @@ class RealTimeCamera {
   setFilter(filterName) {
     this._filterName = filterName;
   }
+
+  snapshot(saveType) {
+    let imageType = 'image/png';
+    let fileName = 'sample.png';
+    if (saveType === 'jpeg') {
+      imageType = 'image/jpeg';
+      fileName = 'sample.jpg';
+    }
+    const base64 = this._canvasElement.toDataURL(imageType);
+    const blob = this._base64toBlob(base64);
+    this._saveBlob(blob, fileName);
+  }
+
+  _base64toBlob(base64) {
+    const tmp = base64.split(',');
+    const data = atob(tmp[1]);
+    const mime = tmp[0].split(':')[1].split(';')[0];
+    const buf = new Uint8Array(data.length);
+    for (let i = 0; i < data.length; i++) {
+      buf[i] = data.charCodeAt(i);
+    }
+    const blob = new Blob([buf], {
+      type: mime,
+    });
+    return blob;
+  }
+
+  _saveBlob(blob, fileName) {
+    const url = (window.URL || window.webkitURL);
+    const dataUrl = url.createObjectURL(blob);
+    const event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    const a = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+    a.href = dataUrl;
+    a.download = fileName;
+    a.dispatchEvent(event);
+  }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -114,5 +151,10 @@ window.addEventListener('DOMContentLoaded', () => {
   const sepiaFilterButton = document.querySelector('.filter-button__sepia');
   sepiaFilterButton.addEventListener('click', () => {
     realTimeCamera.setFilter('sepia');
+  });
+
+  const snapshotButton = document.querySelector('.snapshot-button');
+  snapshotButton.addEventListener('click', () => {
+    realTimeCamera.snapshot();
   });
 });
