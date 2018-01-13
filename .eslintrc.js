0f181f1fdd0e32f376706fe2041535ab4ff4b504
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-01-13 10:10:43 +0900

    Introduce eslint and prettier for quality of code

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'eslint-config-xo-space/esnext',
+    'eslint-config-prettier',
+    'eslint-config-prettier/standard',
+  ],
+  env: {
+    node: true,
+    browser: true,
+  },
+};
